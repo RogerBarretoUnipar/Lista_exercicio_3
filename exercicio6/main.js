de78b847fb000e7a105f9953d20d3b3b6e4346c5
RogerBarretoUnipar <164963528+RogerBarretoUnipar@users.noreply.github.com>
2024-05-28 20:24:17 -0300

    exercicio 6
    funcionando

--- exercicio6/main.js
+++ exercicio6/main.js
@@ -1,2 +1,11 @@
 let btn = document.querySelector("#btn");
 let resultado = document.querySelector("#resultado");
+let kg = document.querySelector("#kg");
+
+function calculaKG()
+{
+    let valorPrato = parseFloat(kg.value * 12);
+    resultado.textContent = "Seu pratinho deu R$" + valorPrato;
+}
+
+btn.addEventListener("click", calculaKG);
