5891d65c263fb57565828869f6b3cfd82dc370f6
RogerBarretoUnipar <164963528+RogerBarretoUnipar@users.noreply.github.com>
2024-05-28 19:35:43 -0300

    Exercicio 3
    funcionando

--- exercicio3/main.js
+++ exercicio3/main.js
@@ -1,2 +1,16 @@
 let btn = document.querySelector("#btn");
 let resultado = document.querySelector("#resultado");
+let paes = document.querySelector("#paes");
+let broas = document.querySelector("#broas");
+
+function calculaGuarda()
+{
+    let lucroPao = parseFloat(paes.value * 0.12);
+    let lucroBroa = parseFloat(broas.value * 1.50);
+    let lucroTotal = lucroBroa + lucroPao;
+    let dezGuardado = lucroTotal * 0.1;
+
+    resultado.innerHTML = "<br>" + "Lucro do pão: " + lucroPao + "<br>" + "Lucro da Broa: " + lucroBroa +  "<br>" + "lucro Total: " + lucroTotal +  "<br>" + "Valor a ser guardado: " + dezGuardado
+}
+
+btn.addEventListener("click", calculaGuarda);
