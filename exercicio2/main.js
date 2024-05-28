acfea5c94767f2cd614b2cf5ad0f63721c60a74e
RogerBarretoUnipar <164963528+RogerBarretoUnipar@users.noreply.github.com>
2024-05-28 19:14:39 -0300

    exercicio 2
    funcionando

--- exercicio2/main.js
+++ exercicio2/main.js
@@ -1,2 +1,12 @@
 let btn = document.querySelector("#btn");
 let resultado = document.querySelector("#resultado");
+let cavalos = document.querySelector("#cavalos");
+
+
+function calculaFerradura()
+{
+    let ferraduras = cavalos.value * 4;
+    resultado.textContent = "Será necessário " + ferraduras + " Ferraduras";
+}
+
+btn.addEventListener("click", calculaFerradura);
