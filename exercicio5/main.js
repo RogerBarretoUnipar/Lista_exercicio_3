2506eda43c8ca0e38a3a807d0dfe922a1b0ab1e3
RogerBarretoUnipar <164963528+RogerBarretoUnipar@users.noreply.github.com>
2024-05-28 20:18:59 -0300

    exercicio 5
    funcionando

--- exercicio5/main.js
+++ exercicio5/main.js
@@ -1,2 +1,12 @@
 let btn = document.querySelector("#btn");
 let resultado = document.querySelector("#resultado");
+let preco = document.querySelector("#preco");
+let pagamento = document.querySelector("#pagamento");
+
+function botaCem()
+{
+    let litros = parseInt(pagamento.value / preco.value);
+    resultado.textContent = "Você abasteceu " + litros + "L por " + pagamento.value;
+}
+
+btn.addEventListener("click", botaCem);
