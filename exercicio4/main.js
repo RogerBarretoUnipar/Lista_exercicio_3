5c98141de5e4618cb5a0701ee545c785d8c9d2a2
RogerBarretoUnipar <164963528+RogerBarretoUnipar@users.noreply.github.com>
2024-05-28 20:07:17 -0300

    exercicio 4
    funcionando

--- exercicio4/main.js
+++ exercicio4/main.js
@@ -1,2 +1,12 @@
 let btn = document.querySelector("#btn");
 let resultado = document.querySelector("#resultado");
+let nome = document.querySelector("#nome");
+let idade = document.querySelector("#idade");
+
+function diasVida()
+{
+    let diasVivivdos = parseInt(idade.value * 365);
+    resultado.textContent = nome.value + " você já viveu " + diasVivivdos + " dias";
+}
+
+btn.addEventListener("click", diasVida);
