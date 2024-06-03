ff8e12b1a8e770cc6eef6f96cd8fb62b79edd830
RogerBarretoUnipar <164963528+RogerBarretoUnipar@users.noreply.github.com>
2024-06-03 20:27:02 -0300

    exercicio 7
    funcionando

--- exercicio7/main.js
+++ exercicio7/main.js
@@ -1,2 +1,11 @@
 let btn = document.querySelector("#btn");
 let resultado = document.querySelector("#resultado");
+let dia = document.querySelector("#dia");
+let mes = document.querySelector("#mes");
+
+function fazDias() {
+    let dias = (parseInt(mes.value) * 30) + parseInt(dia.value);
+    resultado.textContent = "Já se passaram " + dias + " Dias";
+}
+
+btn.addEventListener("click", fazDias);
