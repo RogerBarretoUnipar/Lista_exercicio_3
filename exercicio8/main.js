376551eedc800b6cf7d2ef88b03b3713ac48e8a6
RogerBarretoUnipar <164963528+RogerBarretoUnipar@users.noreply.github.com>
2024-06-03 20:41:40 -0300

    exercicio 8
    funcionando

--- exercicio8/main.js
+++ exercicio8/main.js
@@ -1,2 +1,21 @@
 let btn = document.querySelector("#btn");
 let resultado = document.querySelector("#resultado");
+let P = document.querySelector("#P");
+let M = document.querySelector("#M");
+let G = document.querySelector("#G");
+
+function encomendaPeca()
+{
+    let valorP = parseFloat(P.value * 10);
+    let valorM = parseFloat(M.value * 12);
+    let valorG = parseFloat(G.value * 15);
+
+    let valorTotal = valorP + valorM + valorG;
+    resultado.innerHTML = "Detalhes da sua encomenda: <br>";
+    resultado.innerHTML += "- Pequeno (P): R$" + valorP.toFixed(2) + "<br>";
+    resultado.innerHTML += "- Médio (M): R$" + valorM.toFixed(2) + "<br>";
+    resultado.innerHTML += "- Grande (G): R$" + valorG.toFixed(2) + "<br>";
+    resultado.innerHTML += "Valor Total: R$" + valorTotal.toFixed(2);
+}
+
+btn.addEventListener("click", encomendaPeca);
