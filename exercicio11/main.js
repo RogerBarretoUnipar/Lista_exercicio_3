ba2fa98a321e36fd9dcf6c25fc36b80aa9dd436d
RogerBarretoUnipar <164963528+RogerBarretoUnipar@users.noreply.github.com>
2024-06-03 21:28:27 -0300

    exercicio 11
    funcioanando

--- exercicio11/main.js
+++ exercicio11/main.js
@@ -1,2 +1,17 @@
 let btn = document.querySelector("#btn");
 let resultado = document.querySelector("#resultado");
+let salario = document.querySelector("#salario");
+
+btn.addEventListener("click", demonizaSalario);
+
+function demonizaSalario() {
+    let salarioInicial = parseFloat(salario.value);
+    let salarioComAumento = salarioInicial * 1.15;
+    let salarioComImposto = salarioComAumento * 0.92; // 100% - 8%
+
+    resultado.textContent = `Salário inicial: R$ ${salarioInicial.toFixed(2)}\n`;
+    resultado.textContent += `Salário com aumento: R$ ${salarioComAumento.toFixed(2)}\n`;
+    resultado.textContent += `Salário final após impostos: R$ ${salarioComImposto.toFixed(2)}`;
+}
+
+btn.addEventListener("click", demonizaSalario);
