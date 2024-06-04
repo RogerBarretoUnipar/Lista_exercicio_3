307a8b5f266ba9a542876cb81ef2d09e7d90bd26
RogerBarretoUnipar <164963528+RogerBarretoUnipar@users.noreply.github.com>
2024-06-03 21:50:53 -0300

    exercicio 13
    toma papai

--- exercicio13/main.js
+++ exercicio13/main.js
@@ -1,2 +1,13 @@
 let btn = document.querySelector("#btn");
 let resultado = document.querySelector("#resultado");
+let raio = document.querySelector("#raio");
+
+function calculaPirraio() {
+    let raioValue = parseFloat(raio.value);
+    let raioduplicado = raioValue * raioValue;
+    let areacaraio = raioduplicado * Math.PI;
+    
+    resultado.textContent = "TUA PIZZA TEM UMA AREA DE " + areacaraio.toFixed(2);
+}
+
+btn.addEventListener("click", calculaPirraio);
